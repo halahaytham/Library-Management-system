14ad8a28b9086956a72025bee6b41e7fb9a4573e
hala Albayoumi <halah2004@gmail.com>
2026-06-16 21:57:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using Library_Management_System.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library_Management_System
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            var app = builder.Build();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Book}/{action=Index}/{id?}"
+            );
+            app.Run();
+        }
+    }
+}
